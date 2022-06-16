7acd0022bd9d76dfa4a8e8c3dccc88eae064722a
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-16 22:28:40 +0000

    Working on compiling CTranslate2

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd deps/CTranslate2/build/
+cmake .. -DOPENMP_RUNTIME=COMP
+
+
--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-git clone https://github.com/OpenNMT/CTranslate2.git deps/CTranslate2/
+# Setup CTranslate2
+git clone --recursive https://github.com/OpenNMT/CTranslate2.git deps/CTranslate2/
+sudo apt-get install -y cmake
+mkdir deps/CTranslate2/build
