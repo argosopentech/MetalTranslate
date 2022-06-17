2b452428c82f547f59443d951d789ee0c9637960
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-17 12:22:57 +0000

    Building tokenizer

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 cd deps/CTranslate2/build/
 cmake .. -DWITH_MKL=OFF -DOPENMP_RUNTIME=NONE
 make -j4
+cd ../../../
+
+cd deps/Tokenizer/build/
+cmake ..
+make
--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 git clone --recursive https://github.com/OpenNMT/CTranslate2.git deps/CTranslate2/
 sudo apt-get install -y cmake
 mkdir deps/CTranslate2/build
+
+# Setup OpenNMT Tokenizer
+git clone --recursive https://github.com/OpenNMT/Tokenizer.git deps/Tokenizer
+sudo apt-get install -y libicu-dev
+mkdir deps/Tokenizer/build
