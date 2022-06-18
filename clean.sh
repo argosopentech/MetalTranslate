8551ce22e661e5c4889eaa7db494fa387215c88f
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-18 19:16:36 +0000

    Clean up setup.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,1 @@
 rm -rf build
-rm -rf deps/CTranslate2/build
-rm -rf deps/Tokenizer/build
--- setup.sh
+++ setup.sh
@@ -2,9 +2,7 @@
 git clone --recursive https://github.com/OpenNMT/CTranslate2.git deps/CTranslate2/
 sudo apt-get install -y cmake
 sudo apt-get install -y libdnnl-dev
-mkdir deps/CTranslate2/build
 
 # Setup OpenNMT Tokenizer
 git clone --recursive https://github.com/OpenNMT/Tokenizer.git deps/Tokenizer
 sudo apt-get install -y libicu-dev
-mkdir deps/Tokenizer/build
