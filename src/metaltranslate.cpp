31071a7de1a2a3d9a25404f754f1e56ed7d2c08d
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-18 19:32:58 +0000

    Connected tokenizer and CTranslate2

--- src/metaltranslate.cpp
+++ src/metaltranslate.cpp
@@ -3,10 +3,12 @@
 #include <onmt/Tokenizer.h>
 
 int main() {
+  std::string source_str("Hello world, we are translating with MetalTranslate.");
+
   onmt::Tokenizer tokenizer(onmt::Tokenizer::Mode::Conservative,
 		  onmt::Tokenizer::Flags::JoinerAnnotate);
   std::vector<std::string> tokens;
-  tokenizer.tokenize("Hello World!", tokens);
+  tokenizer.tokenize(source_str, tokens);
   for(const auto& token: tokens) std::cout << token << ' ';
   std::cout << std::endl;
 
@@ -18,7 +20,8 @@ int main() {
                                          "models/test/",
                                          ctranslate2::Device::CPU);
 
-  const std::vector<std::vector<std::string>> batch = {{"▁H", "ello", "▁world", "!"}};
+  // const std::vector<std::vector<std::string>> batch = {{"▁H", "ello", "▁world", "!"}};
+  const std::vector<std::vector<std::string>> batch = {tokens};
   const std::vector<ctranslate2::TranslationResult> results = translator.translate_batch(batch);
 
   for (const auto& token : results[0].output())
