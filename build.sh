388ed13c9e6152413a6f64c52567551fca369bd1
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-18 17:38:22 +0000

    Building with CMake

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
-mkdir -p deps/CTranslate2/build
-cd deps/CTranslate2/build/
-cmake .. -DWITH_MKL=OFF -DOPENMP_RUNTIME=NONE -DWITH_DNNL=ON
-make -j4
-sudo make install
-cd ../../../
-
-mkdir -p deps/Tokenizer/build
-cd deps/Tokenizer/build/
-cmake ..
-make
-sudo make install
-cd ../../..
-
 mkdir -p build
 cd build
-export LD_LIBRARY_PATH=/usr/local/lib/
-g++ ../src/metaltranslate.cpp ../deps/CTranslate2/build/libctranslate2.so ../deps/Tokenizer/build/libOpenNMTTokenizer.so -o metaltranslate
-cd ..
+cmake .. -DWITH_MKL=OFF -DOPENMP_RUNTIME=NONE -DWITH_DNNL=ON
+make
+
