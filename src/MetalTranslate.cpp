3988993432f817bac654066f04645066111c29ec
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-23 12:59:04 +0000

    Refactor MetalTranslate.cpp

--- src/MetalTranslate.cpp
+++ src/MetalTranslate.cpp
@@ -4,43 +4,62 @@
 #include <iostream>
 #include <onmt/Tokenizer.h>
 
-int main() {
-  std::string source_str(
-      "Hello world, we are translating with MetalTranslate.");
+namespace MetalTranslate {
 
-  std::string model_dir("models/translate-fairseq_m2m_100_418M/");
+MetalTranslate::MetalTranslate(MetalTranslateConfig config) {
+  this->_config = config;
+}
+
+std::string MetalTranslate::Translate(std::string source,
+                                      std::string source_code,
+                                      std::string target_code) {
 
   // Tokenizer
-  onmt::Tokenizer tokenizer(model_dir + "sentencepiece.model");
+  onmt::Tokenizer tokenizer(this->_config.ModelPath + "sentencepiece.model");
   std::vector<std::string> tokens;
-  tokenizer.tokenize(source_str, tokens);
+  tokenizer.tokenize(source, tokens);
 
-  std::string source_prefix = "__en__";
+  std::string source_prefix = "__" + source_code + "__";
   tokens.insert(tokens.begin(), source_prefix);
-
-  std::cout << "Tokens:" << std::endl;
-  for (const auto &token : tokens)
-    std::cout << token << ' ';
-  std::cout << std::endl;
 
   // CTranslate2
   const size_t num_translators = 1;
   const size_t num_threads_per_translator = 4;
   ctranslate2::TranslatorPool translator(
-      num_translators, num_threads_per_translator, model_dir + "model",
-      ctranslate2::Device::CPU);
+      num_translators, num_threads_per_translator,
+      this->_config.ModelPath + "model", ctranslate2::Device::CPU);
 
   const std::vector<std::vector<std::string>> batch = {tokens};
-  const std::vector<std::vector<std::string>> target_prefix = {{"__es__"}};
+  const std::vector<std::vector<std::string>> target_prefix = {
+      {"__" + target_code + "__"}};
   const int max_batch_size = 2024;
 
   const std::vector<ctranslate2::TranslationResult> results =
       translator.translate_batch(batch, target_prefix);
 
-  std::cout << "Translation Result:" << std::endl;
+  const std::vector<std::string> translatedTokens = results[0].output();
 
-  for (const auto &token : results[0].output())
-    std::cout << token << ' ';
-  std::cout << std::endl;
+  std::string result = tokenizer.detokenize(translatedTokens);
+
+  // Remove target prefix
+  // __es__ Traducción de texto con MetalTranslate
+  // -> Traducción de texto con MetalTranslate
+  result = result.substr(7);
+
+  return result;
+}
+
+} // namespace MetalTranslate
+
+int main() {
+  MetalTranslate::MetalTranslateConfig config;
+
+  MetalTranslate::MetalTranslate translate(config);
+
+  std::string result =
+      translate.Translate("Translating text with MetalTranslate", "en", "es");
+
+  std::cout << "Result: " << result << std::endl;
+
   return 0;
 }
--- src/MetalTranslate.h
+++ src/MetalTranslate.h
@@ -1,1 +1,18 @@
-class MetalTranslate {};
+#include <string>
+
+namespace MetalTranslate {
+class MetalTranslateConfig {
+public:
+  std::string ModelPath = "models/translate-fairseq_m2m_100_418M/";
+};
+
+class MetalTranslate {
+public:
+  MetalTranslate(MetalTranslateConfig config);
+  std::string Translate(std::string source, std::string source_code,
+                        std::string target_code);
+
+private:
+  MetalTranslateConfig _config;
+};
+} // namespace MetalTranslate
