57f03d1ddc10f7b9a5e3252546d72ae55cd81ef4
P.J. Finlay <pjappdevelopment@gmail.com>
2022-06-23 12:36:00 +0000

    Auto download M2M-100 model

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+# Download M2M-100 model
+wget -P models/ https://cdn.argosopentech.io/translate-fairseq_m2m_100_418M.argosmodel
+unzip models/translate-fairseq_m2m_100_418M.argosmodel -d models/
+
 # Setup CTranslate2
 git clone --recursive https://github.com/OpenNMT/CTranslate2.git deps/CTranslate2/
 sudo apt-get install -y cmake
